cced1e591025bb034f76faa1861003f24fe0d7ee
Norbert Binkiewicz <norbert@stat.wisc.edu>
2014-08-02 16:13:17 -0500

    Create file with the main function for casc.

---
+++ R/casc.R
@@ -0,0 +1,15 @@
+# casc
+#'
+#' @param adjMat An adjacency matrix
+#' @param covMat A covariate matrix
+#' @param K The number of clusters 
+#'
+#' @export
+#' @return A list with node cluster assignments, the
+#' the value of the tuning parameter used, the within
+#' cluster sum of squares.
+#'
+#' @keywords spectral clustering
+casc = function(adjMat, covMat, K) {
+
+}
