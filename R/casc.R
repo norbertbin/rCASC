06f1486814180e624b74ae623be4129aa94030dc
Norbert Binkiewicz <norbert@stat.wisc.edu>
2014-08-02 16:32:44 -0500

    Added ability to choose form of adjacency matrix.

--- R/casc.R
+++ R/casc.R
@@ -1,8 +1,12 @@
 # casc
-#'
+#' Covariate Assisted Spectral Clustering
+#' 
 #' @param adjMat An adjacency matrix
 #' @param covMat A covariate matrix
-#' @param K The number of clusters 
+#' @param K The number of clusters
+#' @param method The form of the adjacency matrix to be used.
+#' @param rowNorm True if row normalization should be
+#' done before running kmeans.
 #'
 #' @export
 #' @return A list with node cluster assignments, the
@@ -10,6 +14,35 @@
 #' cluster sum of squares.
 #'
 #' @keywords spectral clustering
-casc = function(adjMat, covMat, K) {
+casc <- function(adjMat, covMat, K, method = "regLaplacian",
+                 rowNorm = TRUE) {
+    
+}
 
+# ---------------------------------------------------------------------
+# Helper methods
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# returns the graph matrix corresponding to the given method
+# ---------------------------------------------------------------------
+getGraphMatrix = function(adjacencyMat, method) {
+
+    if(method == "regLaplacian") {
+        rSums = rowSums(adjacencyMat)
+        tau = mean(rSums)
+        normMat = Diagonal(length(rSums), 1/sqrt(rSums + tau))
+        return(normMat %*% adjacencyMat %*% normMat)
+    }
+    else if(method == "laplacian") {
+        rSums = rowSums(adjacencyMat)
+        normMat = Diagonal(length(rSums), 1/sqrt(rSums))
+        return(normMat %*% adjacencyMat %*% normMat)
+    }
+    else if(method == "adjacency"){
+        return(adjacencyMat)
+    }
+    else {
+        stop(paste("Error: method =", method, "Not valid"))
+    }
 }
