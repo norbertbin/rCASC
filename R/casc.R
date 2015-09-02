0240e51dc28cb841cbfb7a6a158f023464286945
Norbert Binkiewicz <norbert@stat.wisc.edu>
2015-09-01 21:52:48 -0700

    updated namespace

--- R/casc.R
+++ R/casc.R
@@ -4,7 +4,7 @@
 #' @param adjMat An adjacency matrix
 #' @param covMat A covariate matrix
 #' @param nBlocks The number of clusters
-#' @param nIter Number of iterations to find the optimal tuning
+#' @param nPoints Number of iterations to find the optimal tuning
 #' parameter.
 #' @param method The form of the adjacency matrix to be used.
 #' @param rowNorm True if row normalization should be
@@ -12,7 +12,10 @@
 #' @param enhancedTuning If true, then the enhanced tuning procedure is used.
 #' @param center A boolean indicating if the covariate matrix columns
 #' should be centered.
-#' 
+#' @param verbose A boolean indicating if casc output should include eigendecomposition.
+#' @param assortative A boolean indicating if the assortative version of casc should be used.
+#' @param randStarts Number of random restarts for kmeans.
+#' @param epsilon A threshold for identifying subspace discontinuities.
 #'
 #' @export
 #' @return A list with node cluster assignments, the
@@ -20,9 +23,10 @@
 #' cluster sum of squares, and the eigengap.
 #'
 #' @keywords spectral clustering
-casc <- function(adjMat, covMat, nBlocks, nIter = 100,
+casc <- function(adjMat, covMat, nBlocks, nPoints = 100,
                  method = "regLaplacian", rowNorm = F,
-                 enhancedTuning = F, center = F) {
+                 enhancedTuning = F, center = F, verbose = F,
+                 assortative = F, randStarts = 20, epsilon = .05) {
 
     # Matrix has Namespace problems when using dsCMatrix
     adjMat = as(adjMat, "dgCMatrix")
@@ -31,9 +35,68 @@ casc <- function(adjMat, covMat, nBlocks, nIter = 100,
     covMat <- scale(covMat, center = center,
                     scale = sqrt(Matrix::colSums(covMat^2)))
 
-    return( getCascClusters(adjMat, covMat, nBlocks, nIter,
-                            rowNorm, enhancedTuning) )    
+    # return
+    getCascClusters(adjMat, covMat, nBlocks, nPoints,
+                            rowNorm, enhancedTuning, verbose,
+                            assortative, randStarts, epsilon)    
 }
+
+# CCA
+#' A modified CCA approach for spectral clustering.
+#' 
+#' Uses the eigenvectors of adjMat %*% covMat for spectral clustering.
+#' 
+#' @param adjMat An adjacency matrix
+#' @param covMat A covariate matrix
+#' @param nBlocks The number of clusters
+#' @param method The form of the adjacency matrix to be used.
+#' @param rowNorm True if row normalization should be
+#' done before running kmeans.
+#' @param center A boolean indicating if the covariate matrix columns
+#' should be centered.
+#' @param verbose A boolean indicating if casc output should include eigendecomposition.
+#' @param randStarts Number of random restarts for kmeans.
+#'
+#' @export
+#' @return A list with node cluster assignments, the
+#' the value of the tuning parameter used, the within
+#' cluster sum of squares, and the eigengap.
+#'
+#' @keywords spectral clustering
+cca <- function(adjMat, covMat, nBlocks,
+                 method = "regLaplacian", rowNorm = F,
+                 center = F, verbose = F,
+                 randStarts = 10) {
+
+    nCov = ncol(covMat)
+
+    # Matrix has Namespace problems when using dsCMatrix
+    adjMat = as(adjMat, "dgCMatrix")
+    
+    adjMat <- getGraphMatrix(adjMat, method)
+    covMat <- scale(covMat, center = center,
+                    scale = sqrt(Matrix::colSums(covMat^2)))
+
+    ccaSvd = svd(adjMat %*% covMat, nu = min(nBlocks, nCov), nv = 0)
+
+    if(rowNorm == T) {
+        ccaSvd$u = ccaSvd$u/sqrt(colSums(ccaSvd$u^2))
+    }
+
+    kmeansResults = kmeans(ccaSvd$u, nBlocks, nstart = randStarts)
+
+    if(verbose == F) {
+        return(list(cluster = kmeansResults$cluster,
+            wcss = kmeansResults$tot.withinss
+            ))
+    } else {
+        return(list(cluster = kmeansResults$cluster,
+            wcss = kmeansResults$tot.withinss,
+            ccaSvd = ccaSvd
+            ))
+    }
+}
+
 
 # ---------------------------------------------------------------------
 # Helper methods
@@ -67,12 +130,11 @@ getGraphMatrix = function(adjacencyMat, method) {
 # returns CASC optimal h tuning parameter SVD
 # ---------------------------------------------------------------------
 getCascClusters = function(graphMat, covariates, nBlocks,
-    nPoints, rowNorm, enhancedTuning) {
-
-    # value for detecting a transition
-    epsilon = .05
+    nPoints, rowNorm, enhancedTuning, verbose, assortative,
+    randStarts, epsilon) {
     
-    rangehTuning = getTuningRange(graphMat, covariates, nBlocks)
+    rangehTuning = getTuningRange(graphMat, covariates, nBlocks, 
+        assortative)
 
     hTuningSeq = seq(rangehTuning$hmin, rangehTuning$hmax,
         length.out = nPoints)
@@ -83,7 +145,7 @@ getCascClusters = function(graphMat, covariates, nBlocks,
     
     for(i in 1:nPoints) {
         cascResults = getCascResults(graphMat, covariates, hTuningSeq[i],
-            nBlocks, rowNorm)
+            nBlocks, rowNorm, F, assortative, randStarts)
         orthoX[i] = cascResults$orthoX
         orthoL[i] = cascResults$orthoL
         wcssVec[i] = cascResults$wcss
@@ -126,93 +188,150 @@ getCascClusters = function(graphMat, covariates, nBlocks,
     } else {
         hOpt = hTuningSeq[which.min(wcssVec)]
     }
+
+    hOptResults = getCascResults(graphMat, covariates, hOpt, nBlocks, rowNorm, 
+        verbose, assortative, randStarts)
     
-    hOptResults = getCascResults(graphMat, covariates, hOpt, nBlocks, rowNorm)
-    
-    return( list(cluster = hOptResults$cluster,
+    if(verbose == F) {
+        return( list(cluster = hOptResults$cluster,
                  h = hOpt,
                  wcss = hOptResults$wcss,
                  eigenGap = hOptResults$eigenGap) )
+    } else {
+        return( list(cluster = hOptResults$cluster,
+                 h = hOpt,
+                 wcss = hOptResults$wcss,
+                 eigenGap = hOptResults$eigenGap,
+                 cascSvd = hOptResults$cascSvd) )
+    }
 }
 
 # ---------------------------------------------------------------------
 # returns cluster memberships for CASC based clustering takes graphMat
 # ---------------------------------------------------------------------
 getCascResults = function(graphMat, covariates, hTuningParam,
-    nBlocks, rowNorm) {
-
-    randStarts = 10 #number of random starts for kmeans
+    nBlocks, rowNorm, verbose, assortative, randStarts) {
     
-    cascSvd = getCascSvd(graphMat, covariates, hTuningParam, nBlocks)
+    cascSvd = getCascSvd(graphMat, covariates, hTuningParam, nBlocks, assortative)
 
-    ortho = getOrtho(graphMat, covariates, cascSvd$singVec, cascSvd$singVal,
+    ortho = getOrtho(graphMat, covariates, cascSvd$eVec, cascSvd$eVal,
         hTuningParam, nBlocks)
 
     if(rowNorm == T) {
-        cascSvd$singVec = cascSvd$singVec/sqrt(colSums(cascSvd$singVec^2))
+        cascSvd$eVec = cascSvd$eVec/sqrt(colSums(cascSvd$eVec^2))
     }
     
-    kmeansResults = kmeans(cascSvd$singVec, nBlocks, nstart = randStarts)
+    kmeansResults = kmeans(cascSvd$eVec, nBlocks, nstart = randStarts)
     
-    return( list(cluster = kmeansResults$cluster,
-                 wcss = kmeansResults$tot.withinss,
-                 singGap = cascSvd$singVal[nBlocks] -
-                 cascSvd$singVal[nBlocks + 1],
-                 orthoL = ortho$orthoL,
-                 orthoX = ortho$orthoX,
-                 singVecK = cascSvd$singVec[, nBlocks],
-                 singVecKPlus = cascSvd$singVecKPlus) )    
+    if(verbose == F) {
+        return( list(cluster = kmeansResults$cluster,
+            wcss = kmeansResults$tot.withinss,
+            singGap = cascSvd$eVal[nBlocks] -
+                cascSvd$eVal[nBlocks + 1],
+            orthoL = ortho$orthoL,
+            orthoX = ortho$orthoX) )
+    } else {
+         return( list(cluster = kmeansResults$cluster,
+            wcss = kmeansResults$tot.withinss,
+            eigenGap = cascSvd$eVal[nBlocks] -
+                cascSvd$eVal[nBlocks + 1],
+            orthoL = ortho$orthoL,
+            orthoX = ortho$orthoX,
+            cascSvd = cascSvd) )
+    }
 }
 
 # ---------------------------------------------------------------------
 # returns left singular vectors and values for CASC based clustering
 # ---------------------------------------------------------------------
-getCascSvd = function(graphMat, covariates, hTuningParam, nBlocks) {
+getCascSvd = function(graphMat, covariates, hTuningParam, nBlocks, assortative) {
 
-    #insure irlba internal representation is large enough
-    internalDim = max(2*nBlocks, 20)
+    # # define custom matrix vector multiply function
+    # if(assortative == T) {
+    #     matMult = function(x, args) {
+    #         as.numeric(args$graphMat %*% x + 
+    #             args$h * args$cov %*% crossprod(args$cov, x))
+    #     }
+    # } else {
+    #     matMult = function(x, args) {
+    #         as.numeric(args$graphMat %*% (args$graphMat %*% x) + 
+    #             args$h * args$cov %*% crossprod(args$cov, x))
+    #     }
+    # }
 
-    #define a custom matrix vector multiply function
-    matrixMulti = function(aList, aVector, transposeBool) {
-        return( as.vector(aList$graphMat %*% aVector +
-                          aList$hTuningParam * aList$covariates %*%
-                          crossprod(aList$covariates, aVector) ))
-    } 
+    # eDecomp = eigs(matMult, nBlocks + 2, n = nrow(graphMat), which = "LR", 
+    #     args = list(graphMat = graphMat, cov = covariates, h = hTuningParam))
 
-    singDecomp = irlbaMod(list(graphMat = graphMat, covariates = covariates,
-        hTuningParam = hTuningParam), nu = nBlocks + 1, nv = 0,
-        m_b = internalDim, matmul = matrixMulti)
+    # # return
+    # list(eVec = eDecomp$vectors[, 1:nBlocks],
+    #         eVal = eDecomp$values[1:(nBlocks+1)],
+    #         eVecKPlus = eDecomp$vectors[, nBlocks+1])
 
-    return( list(singVec = singDecomp$u[, 1:nBlocks],
-                 singVal = singDecomp$d,
-                 singVecKPlus = singDecomp$u[, nBlocks+1]) ) 
+    # using irlba
+    # define custom matrix vector multiply function
+    if(assortative == T) {
+        matMult = function(A, x, transpose) {
+            as.numeric(graphMat %*% x + 
+                hTuningParam * covariates %*% crossprod(covariates, x))
+        }
+    } else {
+        matMult = function(A, x, transpose) {
+            as.numeric(graphMat %*% (graphMat %*% x) + 
+                hTuningParam * covariates %*% crossprod(covariates, x))
+        }
+    }
+
+    sDecomp = irlba(A = graphMat, nu = nBlocks + 1, nv = 0,
+        m_b = max(20, 2*nBlocks), matmul = matMult)
+
+    # return
+    list(eVec = sDecomp$u[, 1:nBlocks],
+            eVal = sDecomp$d[1:(nBlocks+1)],
+            eVecKPlus = sDecomp$u[, nBlocks+1])
 }
 
 
 # ---------------------------------------------------------------------
 # gets a good range for the tuning parameter in CASC
 # ---------------------------------------------------------------------
-getTuningRange = function(graphMatrix, covariates, nBlocks) {
-    
-    #ensure irlba internal representation is large enough
-    if(nBlocks > 10) {
-        internalDim = 2 * nBlocks
-    }
-    else {
-        internalDim = 20
-    }
+getTuningRange = function(graphMatrix, covariates, nBlocks,
+    assortative) {
 
-    nCov = dim(covariates)[2]
+    nCov = ncol(covariates)
     
-    singValGraph = irlba(graphMatrix, nu = nBlocks + 1, nv = 0, m_b =
-        internalDim)$d
     singValCov = svd(covariates, nu = min(nBlocks, nCov))$d
 
-    hmax = singValGraph[1]/singValCov[min(nBlocks, nCov)]^2
+    if(assortative == T) {
+        # eigenValGraph = eigs(graphMatrix, nBlocks + 2, which = "LR",
+        #     opts = list(retvec = F))$values #eigenvalues only
 
-    hmin = (singValGraph[nBlocks] - singValGraph[nBlocks + 1])/singValCov[1]^2
+        eigenValGraph = irlba(graphMatrix, nu = nBlocks + 1, nv = 0, 
+            m_b = max(20, 2*nBlocks))$d
 
-    return( list( hmax = hmax, hmin = hmin ) )
+        if(nCov > nBlocks) {
+            hmax = eigenValGraph[1]/(singValCov[nBlocks]^2 - singValCov[nBlocks+1]^2) 
+        } else {
+            hmax = eigenValGraph[1]/singValCov[nCov]^2 
+        }
+        hmin = (eigenValGraph[nBlocks] - eigenValGraph[nBlocks + 1])/singValCov[1]^2
+    } else {
+        # eigenValGraph = eigs(graphMatrix, nBlocks + 2,
+        #     opts = list(retvec = F))$values #eigenvalues only
+        # eigenValGraph = sort(eigenValGraph^2, decreasing=T)
+
+        eigenValGraph = (irlba(graphMatrix, nu = nBlocks + 1, nv = 0, 
+            m_b = max(20, 2*nBlocks))$d)^2
+
+        if(nCov > nBlocks) {
+            hmax = eigenValGraph[1]/(singValCov[nBlocks]^2 - singValCov[nBlocks+1]^2) 
+        } else {
+            hmax = eigenValGraph[1]/singValCov[nCov]^2 
+        }
+        hmin = (eigenValGraph[nBlocks] - eigenValGraph[nBlocks + 1])/singValCov[1]^2
+    }
+
+    # return
+    list( hmax = hmax, hmin = hmin )
 }
 
 # ---------------------------------------------------------------------
@@ -251,13 +370,13 @@ getSubspaces = function(orthoX, orthoL, nPoints, epsilon) {
 # ---------------------------------------------------------------------
 # returns the proportion of the eigenvalues due to X in the top eigenspace
 # ---------------------------------------------------------------------
-getOrtho <- function(graphMat, covariates, cascSvdSingVec, cascSvdSingVal,
+getOrtho <- function(graphMat, covariates, cascSvdEVec, cascSvdEVal,
                      h, nBlocks) {
-    orthoL <- as.numeric((t(cascSvdSingVec[, nBlocks])%*%graphMat%*%
-           cascSvdSingVec[, nBlocks])/cascSvdSingVal[nBlocks])
-    orthoX <- as.numeric(h*(t(cascSvdSingVec[, nBlocks])%*%covariates%*%
-                          t(covariates)%*%cascSvdSingVec[, nBlocks])/
-                       cascSvdSingVal[nBlocks])
+    orthoL <- as.numeric((t(cascSvdEVec[, nBlocks])%*%graphMat%*%
+           cascSvdEVec[, nBlocks])/cascSvdEVal[nBlocks])
+    orthoX <- as.numeric(h*(t(cascSvdEVec[, nBlocks])%*%covariates%*%
+                          t(covariates)%*%cascSvdEVec[, nBlocks])/
+                       cascSvdEVal[nBlocks])
     return( list(orthoL = orthoL/(orthoL + orthoX),
                  orthoX = orthoX/(orthoL + orthoX)) )
 }
